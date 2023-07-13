cfec199601fa01788eaaafe954f31b2de835e7e2
Robert Musili <robersi366@gmail.com>
2023-07-13 11:25:33 +0300

    Done

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
 // Write your code here
+let num1 = 2
+let num2 = 31
+
+if (typeof num1 === 'number' && typeof num2 === 'number'){
+    let multiply = num1 * num2
+}
+
+let random = 9;
+Math.random()
+
+let num3 = 10
+let num4 = 6
+let mod = 10 % 6
+
+let max = Math.max(1,2,3,4,18,19,20)
+
